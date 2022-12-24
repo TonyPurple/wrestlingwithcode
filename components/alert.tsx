ecf4c51321db28550fea75136286eb3a0f242c0f
Anthony Vanoni <avanoni@acromedia.com>
2022-12-24 11:26:44 -0800

    rearrange items

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,34 +1,33 @@
-import Container from './container'
-import cn from 'classnames'
+import Container from "./container";
+import cn from "classnames";
 
 type Props = {
-  preview?: boolean
-}
+  preview?: boolean;
+};
 
 const Alert = ({ preview }: Props) => {
   return (
     <div
-      className={cn('border-b', {
-        'bg-neutral-800 border-neutral-800 text-white': preview,
-        'bg-neutral-50 border-neutral-200': !preview,
+      className={cn("border-b", {
+        "bg-neutral-800 border-neutral-800 text-white": preview,
+        "bg-neutral-50 border-neutral-200": !preview,
       })}
     >
       <Container>
         <div className="py-2 text-center text-sm">
-            <>
-              If you are interested in continuing the conversation{' '}
-              <a
-                href={`https://www.linkedin.com/in/anthony-vanoni/`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                connect with me on LinkedIn.
-              </a>
-              .
-            </>
+          <>
+            View some of the bootcamp projects that helped get me hired{" "}
+            <a
+              href={`https://github.com/TonyPurple`}
+              className="underline hover:text-blue-600 duration-200 transition-colors"
+            >
+              on GitHub.
+            </a>
+          </>
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Alert
+export default Alert;
--- components/footer.tsx
+++ components/footer.tsx
@@ -10,10 +10,10 @@ const Footer = () => {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href={`https://github.com/TonyPurple`}
+              href={`https://www.linkedin.com/in/anthony-vanoni/`}
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Visit GitHub
+              Connect on LinkedIn
             </a>
           </div>
         </div>
